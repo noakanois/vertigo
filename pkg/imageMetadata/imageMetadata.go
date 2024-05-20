ccf234a11f198c6d55dca9ae932214595a8abfd7
lietzdaniel <lietz_daniel@gmx.de>
2024-05-20 23:59:33 +0200

    added description to errors

--- pkg/imageMetadata/imageMetadata.go
+++ pkg/imageMetadata/imageMetadata.go
@@ -1,6 +1,7 @@
 package imageMetadata
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -10,17 +11,17 @@ import (
 func GetImageCreationDate(imagePath string) (creationDate time.Time, Error error) {
 	file, err := os.Open(imagePath)
 	if err != nil {
-		return time.Time{}, err
+		return time.Time{}, fmt.Errorf("error opening image: %v", err)
 	}
 
 	ExifMetadata, err := exif.Decode(file)
 	if err != nil {
-		return time.Time{}, err
+		return time.Time{}, fmt.Errorf("error decoding exif metadata: %v", err)
 	}
 
 	createdDate, err := ExifMetadata.DateTime()
 	if err != nil {
-		return time.Time{}, err
+		return time.Time{}, fmt.Errorf("error extracting date: %v", err)
 	}
 
 	return createdDate, nil
@@ -29,17 +30,17 @@ func GetImageCreationDate(imagePath string) (creationDate time.Time, Error error
 func GetImageLocation(imagePath string) (latitude float64, longitude float64, Error error) {
 	file, err := os.Open(imagePath)
 	if err != nil {
-		return 0.0, 0.0, err
+		return 0.0, 0.0, fmt.Errorf("error opening image: %v", err)
 	}
 
 	ExifMetadata, err := exif.Decode(file)
 	if err != nil {
-		return 0.0, 0.0, err
+		return 0.0, 0.0, fmt.Errorf("error decoding exif metadata: %v", err)
 	}
 
 	lat, long, err := ExifMetadata.LatLong()
 	if err != nil {
-		return 0.0, 0.0, err
+		return 0.0, 0.0, fmt.Errorf("error extracting location: %v", err)
 	}
 
 	return lat, long, nil
