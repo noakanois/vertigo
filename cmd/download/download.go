e4b1ee8c8454e1bc63bb7e5c394e67eb70f65345
noah <noakanois@gmail.com>
2024-05-25 13:17:03 +0200

    go python

---
+++ cmd/download/download.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+
+	"vertigo/pkg/stockx"
+)
+
+func main() {
+	stockx.GetVisualItem("3", "https://images.stockx.com/images/Nike-Air-Max-1-97-Sean-Wotherspoon-Product.jpg")
+}
